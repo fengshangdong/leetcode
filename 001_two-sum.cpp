3a8e84295c64b3db8f34893ba635af512bb43a83
fengshangdong <fengshangdong@outlook.com>
2018-10-29 04:05:47 +0800

    简单数组

---
+++ 001_two-sum.cpp
@@ -0,0 +1,40 @@
+#include<map>
+#include<vector>
+#include<cstdio>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+		map<int, int> hash;
+		vector<int> result;
+		/* key - value */
+		for (int i = 0; i < nums.size(); i++) {
+			int numberToFind = target - nums[i];
+			if (hash.find(numberToFind) != hash.end()) {
+				result.push_back(hash[numberToFind]);
+				result.push_back(i);
+				return result;
+			}
+			hash[nums[i]] = i;
+		}
+		return result;
+	}
+};
+
+int main()
+{
+	int arr[] = {2,11,7,15};
+	vector<int> vec;
+	for (int i = 0; i < sizeof(arr)/sizeof(arr[0]); i++) {
+		vec.push_back(arr[i]);
+	}
+
+	Solution solve;
+	vector<int> res = solve.twoSum(vec, 9);
+	for (int i = 0; i < res.size(); i++) {
+		printf("%d ", res[i]);
+	}
+	printf("\n");
+}
---
+++ 217_contains-duplicate.cpp
@@ -0,0 +1,31 @@
+#include<vector>
+#include<set>
+#include<cstdio>
+
+using namespace std;
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+		set<int> res;
+		for (int i = 0; i < nums.size(); i++) {
+			if (res.find(nums[i]) != res.end()) {
+				return true;
+			}
+			res.insert(nums[i]);
+		}
+		return false;
+	}
+};
+
+int main()
+{
+	int arr[] = {1,2,3,4};
+	vector<int> vec;
+	for (int i = 0; i < sizeof(arr)/sizeof(arr[0]); i++) {
+		vec.push_back(arr[i]);
+	}
+
+	Solution solve;
+	printf("%d\n", solve.containsDuplicate(vec));
+}
